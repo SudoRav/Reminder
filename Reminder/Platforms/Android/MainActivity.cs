d979547899f482123d6aa8271b666bc1e374cb4a
SudoRav <91259187+SudoRav@users.noreply.github.com>
2026-07-22 21:16:00 +0300

    Fix reminder editor period controls

--- Reminder/Platforms/Android/MainActivity.cs
+++ Reminder/Platforms/Android/MainActivity.cs
@@ -8,5 +8,10 @@ namespace Reminder
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, WindowSoftInputMode = SoftInput.AdjustResize)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Window?.SetSoftInputMode(SoftInput.AdjustResize);
+        }
     }
 }
